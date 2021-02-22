96e3c0c5c78b6c80ee2d653cd5e35a88e3c0ab29
tom_glb <goodMoring@163.com>
2021-02-22 15:04:35 +0800

    af1223123123123

--- src/main/java/com/atguigu/git/TestGit.java
+++ src/main/java/com/atguigu/git/TestGit.java
@@ -2,10 +2,7 @@ package com.atguigu.git;
 
 public class TestGit {
 	public static void main(String args[]) {
-		System.out.println("testgit");
-		System.out.println("testgit");
-		System.out.println("testgit");
-		System.out.println("testgit");
+		
 		
 	}
 }
